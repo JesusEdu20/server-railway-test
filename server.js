4274c1de1b379a56acc8b33bef7e6cb3b1b86811
Jesus <jesuseduardocast18@gmail.com>
2024-07-25 08:42:40 -0400

    Add: dotenv

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const app = express()
-const PORT = 8080
+require('dotenv').config()
+const PORT = process.env.PORT || 8080
 
 app.get('/', (req, res)=>{
     res.send('hello world')
 })
+
+
 
 app.listen(PORT, ()=>{console.log(`App listen on port ${PORT}`)})
